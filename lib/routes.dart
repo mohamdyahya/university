4045dee97c1bfcf69eabc7eb1a4f3ec2ff0d9418
mohamdyahya <aboraed19999@gmail.com>
2022-04-21 18:15:57 +0300

    adding main page

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,6 +2,7 @@
 // We use name route
 // All our routes will be available here
 import 'package:flutter/cupertino.dart';
+import 'package:university/screans/Main/MainPage.dart';
 import 'package:university/screans/app_about/app_info_screen.dart';
 import 'package:university/screans/forgot_password/forgot_password_screen.dart';
 import 'package:university/screans/login_success/login_success_screen.dart';
@@ -20,5 +21,5 @@ final Map<String, WidgetBuilder> routes = {
   SignUpScreen.routeName: (context) => SignUpScreen(),
   AppInfoScrean.routeName: (context) => AppInfoScrean(),
   MyReservations.routeName: (context) => MyReservations(),
-  MyUser.routeName: (context) => Reservation(),
+  MyUser.routeName: (context) => Main_Page(),
 };
---
+++ lib/screans/Main/MainPage.dart
@@ -0,0 +1,36 @@
+
+import 'package:flutter/material.dart';
+
+import 'Components/bdoy.dart';
+
+// class MyApp extends StatefulWidget {
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       // home:Text("thunder");
+//       home: Main_Page(),
+//     );
+//   }
+// }
+
+class Main_Page extends StatefulWidget {
+  @override
+  State<Main_Page> createState() => _Main_PageState();
+}
+
+class _Main_PageState extends State<Main_Page> {
+  int selectedindex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: body());
+  }
+}
