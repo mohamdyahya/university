1ff4d2991ed3167650d8c0ea647f2463d49b22c6
mohamdyahya <aboraed19999@gmail.com>
2022-04-22 14:24:12 +0300

    edit routs

--- lib/routes.dart
+++ lib/routes.dart
@@ -6,6 +6,10 @@ import 'package:university/screans/Main/MainPage.dart';
 import 'package:university/screans/app_about/app_info_screen.dart';
 import 'package:university/screans/forgot_password/forgot_password_screen.dart';
 import 'package:university/screans/login_success/login_success_screen.dart';
+import 'package:university/screans/reservations/my_reservations_screen.dart';
+import 'package:university/screans/sign_in/sign_in_screen.dart';
+import 'package:university/screans/sign_up/sign_up_screen.dart';
+import 'package:university/screans/splash/splash_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => SplashScreen(),
